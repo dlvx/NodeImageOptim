de2fa91f20238d3083a82752df4137ad931e4a21
Del Valle, Jose <j.del.valle.cordero@accenture.com>
2016-04-05 09:40:46 -0600

    Added ng-file-upload module, validating file type to upload

--- public/js/app.js
+++ public/js/app.js
@@ -1,54 +1,74 @@
 //Module
-var app = angular.module('app', []);
+var app = angular.module('app', ['ngFileUpload']);
 
 //Directive STUDY
-app.directive('fileModel', ['$parse', function ($parse) {
-            return {
-               restrict: 'A',
-               link: function(scope, element, attrs) {
-                  var model = $parse(attrs.fileModel);
-                  var modelSetter = model.assign;
-
-                  element.bind('change', function(){
-                     scope.$apply(function(){
-                        modelSetter(scope, element[0].files[0]);
-                     });
-                  });
-               }
-            };
-}]);
+// app.directive('fileModel', ['$parse', function ($parse) {
+//             return {
+//                restrict: 'A',
+//                link: function(scope, element, attrs) {
+//                   var model = $parse(attrs.fileModel);
+//                   var modelSetter = model.assign;
+//
+//                   element.bind('change', function(){
+//                      scope.$apply(function(){
+//                         modelSetter(scope, element[0].files[0]);
+//                      });
+//                   });
+//                }
+//             };
+// }]);
 
 //Service
-app.service('uploadService', ['$http', function($http){
-  this.uploadToUrl = function(file, uploadUrl){
-    var fd = new FormData();
-    fd.append('file', file);
-
-    $http.post(uploadUrl, fd, {
-      transformRequest: angular.identity,
-      headers: {'Content-Type':undefined}
-    })
-    .success(function(){
-
-    })
-    .error(function(){
-
-    })
-
-  }
-}])
+// app.service('uploadService', ['$http', function($http){
+//   this.uploadToUrl = function(file, uploadUrl){
+//     var fd = new FormData();
+//     fd.append('file', file);
+//
+//     $http.post(uploadUrl, fd, {
+//       transformRequest: angular.identity,
+//       headers: {'Content-Type':undefined}
+//     })
+//     .success(function(){
+//
+//     })
+//     .error(function(){
+//
+//     })
+//
+//   }
+// }])
 
 //Controller
-app.controller('mainController', ['$scope', 'uploadService', function($scope, uploadService){
+app.controller('mainController', ['$scope', 'Upload', function($scope, Upload){
 
   //upload function
-  $scope.upload = function(){
-    var file = $scope.myFile;
-    console.log('File: ');
-    console.log(file);
+  // $scope.upload = function(){
+  //   var file = $scope.myFile;
+  //   console.log('File: ');
+  //   console.log(file);
+  //
+  //   var uploadUrl = '/upload';
+  //   uploadService.uploadToUrl(file, uploadUrl);
+  // };
 
-    var uploadUrl = '/upload';
-    uploadService.uploadToUrl(file, uploadUrl);
+  $scope.submit = function(){
+    if($scope.form.file.$valid && $scope.file){
+      $scope.upload($scope.file);
+    }
+  };
+
+  $scope.upload = function(file){
+    Upload.upload({
+      url: '/upload',
+      data: { file : file }
+    }).then(function(resp){
+      console.log('Success ' + resp.config.data.file.name + 'uploaded. Response: ' + resp.data);
+    },function(resp){
+      console.log('Error status: ' + resp.status);
+    },function(evt){
+      var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+      console.log('progress: ' + progressPercentage + '% ' + evt.config.data.file.name);
+    });
   };
 
 }]);
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 //Dependencies
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 3002;
+var port = process.env.PORT || 3000;
 
 
 
